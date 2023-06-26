1cefe80b4888f57f2c57c52fba1c7f7c6d642b1c
AndreiParmon <parmonandrei@gmail.com>
2023-06-26 21:11:10 +0300

    created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.

--- note_app.py
+++ note_app.py
@@ -1,8 +1,19 @@
 from os import path
+<<<<<<< HEAD
+=======
+import datetime
+
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
 
 file_base = "base.csv"
 all_data = []
 last_id = 0
+<<<<<<< HEAD
+=======
+datetime = datetime.datetime.today()
+dt = datetime.strftime("%d.%m.%Y %H:%M")
+
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
 
 if not path.exists(file_base):
     with open(file_base, "w", encoding="utf-8") as  _:
@@ -25,8 +36,14 @@ def show_all():
 
 def add_new_entry():
     global last_id
+<<<<<<< HEAD
 
     array = ['header', 'information', 'date_time']
+=======
+    global dt
+
+    array = ['Заголовок', 'Заметка']
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
     answers = []
     for i in array:
         answers.append(data_collection(i))
@@ -85,19 +102,33 @@ def search_entry():
 
 def existing_entry(rec_id, data):
     if rec_id:
+<<<<<<< HEAD
         candidates = [i for i in all_data if rec_id in i.split([0])]
+=======
+        candidates = [i for i in all_data if rec_id in i.split()[0]]
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
     else:
         candidates = [i for i in data if data in i]
     return candidates
 
 def data_collection(num):
     answer = input(f"Введите {num}: ")
+<<<<<<< HEAD
     while True:
         if num in "header information":
             if len(answer) == 1:
                 break
         answer = input(f"Данные неверны!\n"
                        f"В записи должно быть не меньше одного символа!\n"
+=======
+    str = ""
+    while True:
+        if num in "Заголовок Заметка":
+            if not str:
+                break
+        answer = input(f"Данные неверны!\n"
+                       f"В записи должно быть не менее одного символа!\n"
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
                        f"Введите {num}: ")
     return answer
 
@@ -106,14 +137,23 @@ def main_menu():
 
     while play:
         read_records()
+<<<<<<< HEAD
         answer = input("Заметки:\n"
                        "1. Покакзать все записи\n"
+=======
+        answer = input("Меню: \n"
+                       "1. Покакать все записи\n"
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
                        "2. Добавить запись\n"
                        "3. Поиск записи\n"
                        "4. Изменить запись\n"
                        "5. Удалить запись\n"
+<<<<<<< HEAD
                        "6. Импорт/экспорт\n"
                        "7. Выход\n")
+=======
+                       "6. Выход\n")
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
         match answer:
             case "1":
                 show_all()
@@ -128,9 +168,38 @@ def main_menu():
             case "5":
                 del_entry()
             case "6":
+<<<<<<< HEAD
                 exp_imp_menu()
             case "7":
+=======
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
                 play = False
             case _:
                 print("Попробуйте ещё раз!\n")
 
+<<<<<<< HEAD
+=======
+def edit_menu():
+    add_dict = {"1": "Заголовок", "2": "Заметка"}
+
+    show_all()
+    record_id = input("Введите ID: ")
+
+    if existing_entry(record_id, ""):
+        while True:
+            print("\nИзменить:")
+            change = input("1. Заголовок\n"
+                           "2. Заметка\n"
+                           "3. Выход\n")
+            match change:
+                case "1" | "2":
+                    return record_id, change, data_collection(add_dict[change])
+                case "3":
+                    return 0
+                case _:
+                    print("Данные введены не верно.")
+    else:
+        print("Данные не верны!")
+
+main_menu()
+>>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
