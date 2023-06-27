d053c80c2725b3eb9a5bb5dad028cbfb2a499e69
AndreiParmon <parmonandrei@gmail.com>
2023-06-27 21:46:07 +0300

    created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu, edit_menu

--- note_app.py
+++ note_app.py
@@ -1,19 +1,12 @@
 from os import path
-<<<<<<< HEAD
-=======
 import datetime
-
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
 
 file_base = "base.csv"
 all_data = []
 last_id = 0
-<<<<<<< HEAD
-=======
+
 datetime = datetime.datetime.today()
 dt = datetime.strftime("%d.%m.%Y %H:%M")
-
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
 
 if not path.exists(file_base):
     with open(file_base, "w", encoding="utf-8") as  _:
@@ -36,14 +29,9 @@ def show_all():
 
 def add_new_entry():
     global last_id
-<<<<<<< HEAD
-
-    array = ['header', 'information', 'date_time']
-=======
     global dt
 
     array = ['Заголовок', 'Заметка']
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
     answers = []
     for i in array:
         answers.append(data_collection(i))
@@ -104,11 +92,7 @@ def search_entry():
 def existing_entry(rec_id, data):
 
     if rec_id:
-<<<<<<< HEAD
-        candidates = [i for i in all_data if rec_id in i.split([0])]
-=======
         candidates = [i for i in all_data if rec_id in i.split()[0]]
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
     else:
         candidates = [i for i in data if data in i]
     return candidates
@@ -116,14 +100,6 @@ def existing_entry(rec_id, data):
 def data_collection(num):
 
     answer = input(f"Введите {num}: ")
-<<<<<<< HEAD
-    while True:
-        if num in "header information":
-            if len(answer) == 1:
-                break
-        answer = input(f"Данные неверны!\n"
-                       f"В записи должно быть не меньше одного символа!\n"
-=======
     str = ""
     while True:
         if num in "Заголовок Заметка":
@@ -131,7 +107,6 @@ def data_collection(num):
                 break
         answer = input(f"Данные неверны!\n"
                        f"В записи должно быть не менее одного символа!\n"
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
                        f"Введите {num}: ")
     return answer
 
@@ -140,23 +115,13 @@ def main_menu():
     play = True
     while play:
         read_records()
-<<<<<<< HEAD
-        answer = input("Заметки:\n"
-                       "1. Покакзать все записи\n"
-=======
         answer = input("Меню: \n"
                        "1. Покакать все записи\n"
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
                        "2. Добавить запись\n"
                        "3. Поиск записи\n"
                        "4. Изменить запись\n"
                        "5. Удалить запись\n"
-<<<<<<< HEAD
-                       "6. Импорт/экспорт\n"
-                       "7. Выход\n")
-=======
                        "6. Выход\n")
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
         match answer:
             case "1":
                 show_all()
@@ -171,17 +136,10 @@ def main_menu():
             case "5":
                 del_entry()
             case "6":
-<<<<<<< HEAD
-                exp_imp_menu()
-            case "7":
-=======
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
                 play = False
             case _:
                 print("Попробуйте ещё раз!\n")
 
-<<<<<<< HEAD
-=======
 def edit_menu():
 
     add_dict = {"1": "Заголовок", "2": "Заметка"}
@@ -205,4 +163,3 @@ def edit_menu():
         print("Данные не верны!")
 
 main_menu()
->>>>>>> f4a889a (created: read_records, show_all, add_new_entry, del_entry, change_entry, search_entry, existing_entry, mein_menu.)
